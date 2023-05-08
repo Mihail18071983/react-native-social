3e5e7a8051e9bbe05ea14c7527e89f27d569c37b
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-05-08 17:03:14 +0300

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,42 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authReducer";
 
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-
-// import { persistStore, persistReducer } from "redux-persist";
-
-// const persistConfig = {
-//   key: "auth",
-//   storage: AsyncStorage,
-// };
-
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from "redux-persist";
 
 const rootReducer = combineReducers({
   [authSlice.name]: authSlice.reducer,
 });
 
-// export const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
 export const store = configureStore({
   reducer: rootReducer,
 });
 
-// export const persistor = persistStore(store);
 
