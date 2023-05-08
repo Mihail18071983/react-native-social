3c2314748a1e8867df61626cc8b63294edf480f2
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-05-08 17:15:54 +0300

    Remove node_modules folder

--- node_modules/expo-notifications/build/unregisterForNotificationsAsync.js
+++
@@ -1,9 +0,0 @@
-import { UnavailabilityError } from 'expo-modules-core';
-import PushTokenManager from './PushTokenManager';
-export default async function unregisterForNotificationsAsync() {
-    if (!PushTokenManager.unregisterForNotificationsAsync) {
-        throw new UnavailabilityError('ExpoNotifications', 'unregisterForNotificationsAsync');
-    }
-    return PushTokenManager.unregisterForNotificationsAsync();
-}
-//# sourceMappingURL=unregisterForNotificationsAsync.js.map
